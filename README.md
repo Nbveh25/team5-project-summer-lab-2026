8271bd1a8f48ab1f25e77a955cbe9fcce598a075
Бикмухаметов Тимур Ильдарович TiIBikmukhametov <tiibikmukhametov@stud.kpfu.ru>
2026-07-05 17:49:16 +0300

    init: init repository

---
+++ README.md
@@ -0,0 +1,5 @@
+## Developers
+
+1. Круглов Александр Викторович — [@sanyacercl](https://t.me/sanyacercl)
+2. Кондратьев Артём Анатольевич — [@SIUx10](https://t.me/SIUx10)
+3. Овечкин Михаил Николаевич — [@kish777mish777](https://t.me/kish777mish777)
---
+++ app/src/main/java/ru/kpfu/itis/summerlab/team5/shopapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.kpfu.itis.summerlab.team5.shopapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.kpfu.itis.summerlab.team5.shopapp.ui.theme.SummerPractiseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SummerPractiseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SummerPractiseTheme {
+        Greeting("Android")
+    }
+}
